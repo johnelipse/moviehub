f1cfc489d4d8461412c2c94772507cb43d6fde9b
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-07-30 18:59:00 +0300

    slider added

--- app/(frontend)/movielib/page.tsx
+++ app/(frontend)/movielib/page.tsx
@@ -1,10 +1,15 @@
 import Nav2 from '@/components/front/navs/nav2'
+import App from '@/components/front/slider'
+import SwiperElement from '@/components/front/slider'
 import React from 'react'
 
 export default function page() {
   return (
     <div className='bg-white dark:bg-[#020817] px-4 py-4 w-screen h-screen overflow-x-hidden'>
      <Nav2/>
+     <div className='mt-20'>
+      <App/>
+     </div>
    </div>
   )
 }
---
+++ components/front/slider.tsx
@@ -0,0 +1,54 @@
+"use client"
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import { Link } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function App() {
+  return (
+  
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper "
+      >
+          <SwiperSlide>
+            <Link href="/">
+            <Image width={6912} height={3456} src="/images/service.png" alt="banner" />
+            </Link>
+          </SwiperSlide>
+        <SwiperSlide>
+        <Link href="/">
+            <Image width={6912} height={3456} src="/images/Car Wash.png" alt="banner" />
+            </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link href="/">
+            <Image width={6912} height={3456} src="/images/Bold Movie.png" alt="banner" />
+            </Link>
+        </SwiperSlide>
+        </Swiper>
+    
+  );
+}
